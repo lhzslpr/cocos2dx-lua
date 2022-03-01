2af1f0889aaad9271f0fcefa161901d1626a0885
Abrand <351783080@qq.com>
2022-03-01 22:51:46 +0800

    加载主场景节点

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -3,15 +3,14 @@ local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
 function MainScene:onCreate()
     -- add background image
-    display.newSprite("HelloWorld.png")
-        :move(display.center)
-        :addTo(self)
+  
 
-    -- -- add HelloWorld label
-    -- cc.Label:createWithSystemFont("Hello World", "Arial", 10)
-    --     :move(display.cx, display.cy + 200)
-    --     :addTo(self)
     print("log console")
+    self:createResoueceNode("MainScene/MainScene.csb")
+
+    -- display.newSprite("HelloWorld.png")
+    -- :move(display.center)
+    -- :addTo(self.resourceNode_)
 end
 
 return MainScene
